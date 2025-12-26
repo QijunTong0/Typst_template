78e4ed7bb726a3e5cb50059ae5b58ac0850682d6
QijunTong <40225103+QijunTong0@users.noreply.github.com>
2025-12-26 18:44:55 +0800

    Implement OS-specific sed options for spacing
    Add OS detection for sed options in format_compile.sh

--- format_compile.sh
+++ format_compile.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+
+# 使用法: ./latex_spacer.sh <ファイルパス>
+
 if [ -z "$1" ]; then
     echo "Usage: $0 <filename>"
     exit 1
 fi
 
-# Insert spaces before and after inline math(インライン数式の前後にスペースを挿入する)
-sed -i -E 's/(\$[^$]+\$)/ \1 /g; s/  */ /g; s/^ //; s/ $//' "$1"
-
-typstyle -i "$1"
-typst compile "$1"
+# OS判定を行い、sedのオプションを切り替える
+if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS の場合: -i '' を指定しないとエラーになる
+    sed -i '' -E 's/([^[:space:]])(\$[^$]+\$)/\1 \2/g; s/(\$[^$]+\$)([^[:space:]])/\1 \2/g' "$1"
+else
+    # Linux (GNU sed) の場合
+    sed -i -E 's/([^[:space:]])(\$[^$]+\$)/\1 \2/g; s/(\$[^$]+\$)([^[:space:]])/\1 \2/g' "$1"
+fi
